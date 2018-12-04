4a4d589a168631f05444aeeaf3448e31d6db3964
Ryan Biwer <ryan@brightcellars.com>
2018-12-03 23:56:34 -0500

    Upgrade

--- ppx/ppx_decco.sh
+++ ppx/ppx_decco.sh
@@ -1,2 +1,9 @@
-#!/usr/bin/env bash
-${BASH_SOURCE%/*}/../_build/default/.ppx/ppx_decco/ppx.exe --as-ppx $@
+#!/usr/bin/env node
+const { spawn } = require("child_process");
+const path = require("path");
+
+const child = spawn(
+    path.resolve(__dirname, "../_build/default/.ppx/ppx_decco/ppx.exe"),
+    [ "--as-ppx", ...process.argv.slice(2) ],
+    { stdio: "inherit" }
+);
