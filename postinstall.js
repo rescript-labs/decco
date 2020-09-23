62e573bc2a02e4b1c805fa0b4aa4388fc7d910ea
Andrew Herron <thespyder@programmer.net>
2020-09-23 15:22:26 +1000

    Add windows to travis build matrix

--- postinstall.js
+++ postinstall.js
@@ -1,12 +1,15 @@
 const path = require("path");
 const fs = require("fs");
 
-const installBinary = (binary) => {
+const installMacLinuxBinary = (binary) => {
   const source = path.join(__dirname, binary);
   if (fs.existsSync(source)) {
+    // mac and linux support extension-less executables
+    // so just overwrite the shell script
     const target = path.join(__dirname, "ppx")
     fs.renameSync(source, target)
-    // it should be executable in the bundle, but just in case
+    
+    // The ppx should be executable in the bundle, but just in case
     fs.chmodSync(target, 0777)
   } else {
     // assume we're in dev mode - nothing will break if the script
@@ -14,15 +17,34 @@ const installBinary = (binary) => {
   }
 }
 
+const installWindowsBinary = () => {
+  const source = path.join(__dirname, "ppx-windows.exe")
+  if (fs.existsSync(source)) {
+    const target = path.join(__dirname, "ppx.exe")
+    fs.renameSync(source, target)
+
+    // windows scripts use a different file extension to executables
+    // so we delete the script to make sure windows uses the exe now
+    const windowsScript = path.join(__dirname, "ppx.cmd")
+    fs.unlinkSync(windowsScript)
+  } else {
+    // assume we're in dev mode - nothing will break if the script
+    // isn't overwritten, it will just be slower
+  }
+}
+
+
 
 switch (process.platform) {
   case "linux":
-    installBinary("ppx-linux.exe")
-    break;
+    installMacLinuxBinary("ppx-linux.exe")
+    break
   case "darwin":
-    installBinary("ppx-osx.exe")
-    break;
+    installMacLinuxBinary("ppx-osx.exe")
+    break
   case "win32":
+    installWindowsBinary()
+    break
   default:
     // This won't break the installation because the `ppx` shell script remains
     // but that script will throw an error in this case anyway
